e926845023c9288438599ecd190136f34da6ee1c
dghjunior <dghjunior@gmail.com>
2023-04-20 14:22:18 -0400

    Created initial GUI window, added image

---
+++ game_gui.py
@@ -0,0 +1,23 @@
+# import dependencies
+import numpy as np
+import matplotlib
+import tkinter as tk
+import Connect4
+from PIL import ImageTk, Image
+
+class game_gui:
+    def __init__(self):
+        window = tk.Tk()
+        window.geometry("800x800")
+        
+        # Create a photoimage object of the image in the path
+        image1 = Image.open("Connect+4+Background.jpg")
+        test = ImageTk.PhotoImage(image1)
+
+        label1 = tk.Label(image=test)
+        label1.image = test
+        label1.pack()
+
+        window.mainloop()
+        
+gui = game_gui()
