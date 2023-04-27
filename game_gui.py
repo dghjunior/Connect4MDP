274959910b53339d91fab1f221dd103d73f27672
dghjunior <dghjunior@gmail.com>
2023-04-27 11:54:29 -0400

    Fixed representation to allow new game button

--- game_gui.py
+++ game_gui.py
@@ -3,36 +3,7 @@ from tkinter.ttk import *
 from tkinter import *
 from PIL import Image, ImageTk
 
-window = tk.Tk()
-window.geometry('790x720')
-window.configure(bg='white')
-window.title('Connect 4')
 
-button_frame = Frame(window, bg='white')
-button_frame.pack()
-
-canvas_frame=Frame(window, bg='white')
-canvas_frame.pack()
-
-col = tk.IntVar()
-
-button0 = Button(button_frame, text="1", command = lambda: game.drop_piece(0))
-button0.grid(row=0, column=0, padx=40, pady=10)
-button1 = Button(button_frame, text="2", command = lambda: game.drop_piece(1))
-button1.grid(row=0, column=1, padx=38, pady=10)
-button2 = Button(button_frame, text="3", command = lambda: game.drop_piece(2))
-button2.grid(row=0, column=2, padx=38, pady=10)
-button3 = Button(button_frame, text="4", command = lambda: game.drop_piece(3))
-button3.grid(row=0, column=3, padx=38, pady=10)
-button4 = Button(button_frame, text="5", command = lambda: game.drop_piece(4))
-button4.grid(row=0, column=4, padx=38, pady=10)
-button5 = Button(button_frame, text="6", command = lambda: game.drop_piece(5))
-button5.grid(row=0, column=5, padx=38, pady=10)
-button6 = Button(button_frame, text="7", command = lambda: game.drop_piece(6))
-button6.grid(row=0, column=6, padx=38, pady=10)
-
-canvas = Canvas(canvas_frame, bg="white", width=790, height=700)
-canvas.grid(row=1, column=0)
 
 img_refs = []
 
@@ -41,18 +12,66 @@ img_refs = []
 
 class Connect4:
     def __init__(self):
-        self.new_board()
+        self.window = tk.Tk()
         self.turn = 'r'
+        self.new_board()
 
     def new_board(self):
         # create a new board with 'e' for empty in every position
         self.board = [['e' for x in range(7)] for y in range(6)]
+        self.window.destroy()
+
+        self.window = tk.Tk()
+        self.window.geometry('790x720')
+        self.window.configure(bg='white')
+        self.window.title('Connect 4')
+
+        button_frame = Frame(self.window, bg='white')
+        canvas_frame = Frame(self.window, bg='white')
+        self.canvas = Canvas(canvas_frame, bg="white", width=790, height=700)
+
+        button_frame.pack()
+        canvas_frame.pack()
+
+        button0 = Button(button_frame, text="1", command = lambda: self.drop_piece(0))
+        button0.grid(row=0, column=0, padx=40, pady=10)
+        button1 = Button(button_frame, text="2", command = lambda: self.drop_piece(1))
+        button1.grid(row=0, column=1, padx=38, pady=10)
+        button2 = Button(button_frame, text="3", command = lambda: self.drop_piece(2))
+        button2.grid(row=0, column=2, padx=38, pady=10)
+        button3 = Button(button_frame, text="4", command = lambda: self.drop_piece(3))
+        button3.grid(row=0, column=3, padx=38, pady=10)
+        button4 = Button(button_frame, text="5", command = lambda: self.drop_piece(4))
+        button4.grid(row=0, column=4, padx=38, pady=10)
+        button5 = Button(button_frame, text="6", command = lambda: self.drop_piece(5))
+        button5.grid(row=0, column=5, padx=38, pady=10)
+        button6 = Button(button_frame, text="7", command = lambda: self.drop_piece(6))
+        button6.grid(row=0, column=6, padx=38, pady=10)
+
+        self.canvas.grid(row=1, column=0)
+
+        board = Image.open('Connect4Board.png')
+        bgimg = ImageTk.PhotoImage(board)
+
+        # photoimage = ImageTk.PhotoImage(file="Connect4Board.png")
+        self.canvas.create_image(400, 350, image=bgimg)
+        self.canvas.image = bgimg
+
+        self.window.mainloop()
 
     def print_board(self):
         # print the board
         for row in self.board:
             row = '| (' + ') ('.join(row) + ') |'
             print(row)
+
+    def end_screen(self, winner):
+        self.forget()
+        if winner == 'tie':
+            Label(self.window, text="It's a tie!", font=("Arial", 30), bg="white").pack()
+        else:
+            Label(self.window, text="Player " + winner + " wins!", font=("Arial", 30), bg="white").pack()
+        Button(self.window, text="Play again", command=self.new_board).pack()
 
     def drop_piece(self, col):
         # drop a piece in the given column
@@ -75,11 +94,9 @@ class Connect4:
                 winner = 'Red'
             else:
                 winner = 'Yellow'
-            self.forget()
-            Label(window, text="Player " + winner + " wins!", font=("Arial", 30), bg="white").pack()
+            self.end_screen(winner)
         elif self.check_tie():
-            print('Tie!')
-            self.forget()
+            self.end_screen('tie')
 
     def check_win(self):
         # check if the game is won
@@ -125,9 +142,9 @@ class Connect4:
         x = 123 + 92*col
         y = 95 + 92*row
 
-        canvas.create_image(x, y, image=render, tags=("pieces"))
+        self.canvas.create_image(x, y, image=render, tags=("pieces"))
         img_refs.append(render)
-        canvas.tag_lower("pieces")
+        self.canvas.tag_lower("pieces")
 
     def playYellow(self, row, col):
         load = Image.open('yellow.png')
@@ -136,26 +153,26 @@ class Connect4:
         x = 123 + 92*col
         y = 95 + 92*row
 
-        canvas.create_image(x, y, image=render, tags=("pieces"))
+        self.canvas.create_image(x, y, image=render, tags=("pieces"))
         img_refs.append(render)
-        canvas.tag_lower("pieces")
+        self.canvas.tag_lower("pieces")
 
     def forget(self):
-        for widgets in window.winfo_children():
+        for widgets in self.window.winfo_children():
             widgets.destroy()
 
-board = Image.open('Connect4Board.png')
-bgimg = ImageTk.PhotoImage(board)
+#board = Image.open('Connect4Board.png')
+#bgimg = ImageTk.PhotoImage(board)
 
-photoimage = ImageTk.PhotoImage(file="Connect4Board.png")
-canvas.create_image(400, 350, image=photoimage)
+#photoimage = ImageTk.PhotoImage(file="Connect4Board.png")
+#canvas.create_image(400, 350, image=photoimage)
 
 # Button(window, text="Red", command = showRed).pack()
 # Button(window, text="Yellow", command = showYellow).pack()
 
 game = Connect4()
 
-window.mainloop()
+# window.mainloop()
 
 # x for left = 123, y for top = 95
 # increase x by 92 for each column
